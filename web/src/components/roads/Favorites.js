34635007505e9361e9f37a8a9823cb6f71504e1e
Beñat Barrueta <benat.barrueta@opendeusto.es>
2024-11-03 17:34:58 +0100

    format the favorite class

--- web/src/components/roads/Favorites.js
+++ web/src/components/roads/Favorites.js
@@ -12,23 +12,23 @@ const Favorites = () => {
                 Authorization: `Bearer ${localStorage.getItem('token')}`,
             },
         })
-        .then(response => {
-            const roadPromises = response.data.map(favorite =>
-                axios.get(`/api/roads/id/${favorite.roadID}`)
-                .then(res => res.data)
-            );
+            .then(response => {
+                const roadPromises = response.data.map(favorite =>
+                    axios.get(`/api/roads/id/${favorite.roadID}`)
+                        .then(res => res.data)
+                );
 
-            Promise.all(roadPromises)
-                .then(roads => {
-                    setFavoriteRoads(roads);
-                })
-                .catch(error => {
-                    console.error('Error al obtener los detalles de las carreteras:', error);
-                });
-        })
-        .catch(error => {
-            console.error('Error al obtener los viajes favoritos:', error);
-        });
+                Promise.all(roadPromises)
+                    .then(roads => {
+                        setFavoriteRoads(roads);
+                    })
+                    .catch(error => {
+                        console.error('Error al obtener los detalles de las carreteras:', error);
+                    });
+            })
+            .catch(error => {
+                console.error('Error al obtener los viajes favoritos:', error);
+            });
     }, []);
 
     return (
@@ -43,17 +43,17 @@ const Favorites = () => {
                 ) : (
                     favoriteRoads.map((road) => (
                         <div className="road-card">
-            <h3>{road.nombre}</h3>
-            <p><strong>Tipo:</strong> {road.tipo}</p>
-            <p><strong>Distancia:</strong> {road.distancia}</p>
-            <p><strong>Superficie:</strong> {road.suelo}</p>
-            <p><strong>Provincia:</strong> {road.provincia}</p>
-            <p><strong>Ciudad:</strong> {road.ciudad}</p>
-            <p><strong>Velocidad máxima:</strong> {road.velocidad_max}</p>
-            <Link to={`/roads/${road.id}`} className="details-button">
-                Ver detalles
-            </Link>
-        </div>
+                            <h3>{road.nombre}</h3>
+                            <p><strong>Tipo:</strong> {road.tipo}</p>
+                            <p><strong>Distancia:</strong> {road.distancia}</p>
+                            <p><strong>Superficie:</strong> {road.suelo}</p>
+                            <p><strong>Provincia:</strong> {road.provincia}</p>
+                            <p><strong>Ciudad:</strong> {road.ciudad}</p>
+                            <p><strong>Velocidad máxima:</strong> {road.velocidad_max}</p>
+                            <Link to={`/roads/${road.id}`} className="details-button">
+                                Ver detalles
+                            </Link>
+                        </div>
                     ))
                 )}
             </div>
